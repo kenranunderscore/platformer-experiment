7298e46423e32d4063dec95512ac8aff0f0c1f33
Johannes Maier <johannes.maier@mailbox.org>
2024-01-10 16:30:13 +0100

    flake: overlay package itself onto haskellPackages

--- flake.nix
+++ flake.nix
@@ -6,19 +6,32 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
-        overlays =
-          [ (final: prev: { haskellPackages = prev.haskell.packages.ghc96; }) ];
+        overlays = [
+          (final: prev: { haskellPackages = prev.haskell.packages.ghc96; })
+          self.overlays.default
+        ];
       };
-      platformer = pkgs.haskellPackages.callCabal2nix "platformer"
-        (pkgs.lib.cleanSource ./.) { };
     in {
       devShells.${system}.default = pkgs.haskellPackages.shellFor {
-        packages = _: [ platformer ];
+        packages = p: [ p.platformer ];
         nativeBuildInputs = [
           pkgs.haskellPackages.cabal-fmt
           pkgs.cabal-install
           pkgs.haskellPackages.haskell-language-server
         ];
+      };
+
+      packages.${system}.default = pkgs.haskellPackages.platformer;
+
+      overlays.default = final: prev: {
+        haskellPackages = prev.haskellPackages.override (old: {
+          overrides = final.lib.composeExtensions (old.overrides or (_: _: { }))
+            (hfinal: hprev: {
+              platformer =
+                hfinal.callCabal2nix "platformer" (final.lib.cleanSource ./.)
+                { };
+            });
+        });
       };
     };
 }
