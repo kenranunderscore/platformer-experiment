8bf2c49d9b4ec913e9846a77304474a874adf9c7
Johannes Maier <johannes.maier@mailbox.org>
2024-01-13 14:53:17 +0100

    Add input handling

--- src-lib/Platformer/ECS.hs
+++ src-lib/Platformer/ECS.hs
@@ -1,19 +1,25 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE NoFieldSelectors #-}
 
 module Platformer.ECS (
     Color (..),
     Entity (..),
     Health (..),
+    InputState (..),
     System (..),
+    initialInputState,
     mkEmptyEntity,
+    mkInputSystem,
     mkRenderSystem,
     setAppearance,
     setHealth,
+    setPlayerControlled,
     setPosition,
 ) where
 
+import Control.Concurrent.MVar
 import Control.Monad
 import Data.UUID (UUID)
 import Data.Word (Word8)
@@ -40,13 +46,14 @@ data Entity = Entity
     , health :: Maybe Health
     , appearance :: Maybe Color
     , position :: Maybe (CInt, CInt)
+    , playerControlled :: Bool
     }
     deriving stock (Eq, Ord, Show)
 
 mkEmptyEntity :: IO Entity
 mkEmptyEntity = do
     uuid <- randomIO
-    pure $ Entity uuid Nothing Nothing Nothing
+    pure $ Entity uuid Nothing Nothing Nothing False
 
 setHealth :: Health -> Entity -> Entity
 setHealth health entity = entity{health = Just health}
@@ -57,14 +64,52 @@ setAppearance color entity = entity{appearance = Just color}
 setPosition :: (CInt, CInt) -> Entity -> Entity
 setPosition position entity = entity{position = Just position}
 
-newtype System = System {run :: [Entity] -> IO ()}
+setPlayerControlled :: Entity -> Entity
+setPlayerControlled entity = entity{playerControlled = True}
+
+newtype System = System {run :: [Entity] -> IO [Entity]}
 
 mkRenderSystem :: SDL.Renderer -> SDL.Texture -> System
 mkRenderSystem renderer tileset = System $ \entities ->
-    forM_ entities $ \e -> do
+    forM entities $ \e -> do
         case (e.health, e.appearance, e.position) of
             (Just health, Just color, Just (x, y)) -> do
                 SDL.rendererDrawColor renderer $= toV4 color
                 let rect = SDL.Rectangle (SDL.P $ SDL.V2 x y) (SDL.V2 10 10)
                 SDL.fillRect renderer (Just rect)
             _ -> pure ()
+        pure e
+
+data InputState = InputState
+    { up :: Bool
+    , down :: Bool
+    , left :: Bool
+    , right :: Bool
+    }
+
+initialInputState :: InputState
+initialInputState = InputState False False False False
+
+movementKeyPressed :: InputState -> Bool
+movementKeyPressed s = s.up || s.down || s.left || s.right
+
+mkInputSystem :: IO InputState -> System
+mkInputSystem readInputState = System $ \entities -> do
+    inputState <- readInputState
+    if movementKeyPressed inputState
+        then do
+            forM entities $ \e ->
+                case (e.playerControlled, e.position) of
+                    (True, Just (x, y)) -> do
+                        -- FIXME: dummy implementation only so far
+                        let x'
+                                | inputState.left = if inputState.right then x else x - 1
+                                | inputState.right = x + 1
+                                | otherwise = x
+                        let y'
+                                | inputState.up = if inputState.down then y else y - 1
+                                | inputState.down = y + 1
+                                | otherwise = y
+                        pure $ setPosition (x', y') e
+                    _ -> pure e
+        else pure entities
--- src-lib/Platformer/Main.hs
+++ src-lib/Platformer/Main.hs
@@ -3,10 +3,14 @@
 
 module Platformer.Main (main) where
 
+import Control.Concurrent (threadDelay)
+import Control.Concurrent.MVar
 import Control.Exception.Safe qualified as Exception
 import Control.Monad
 import Data.Foldable (traverse_)
 import Data.Function ((&))
+import Data.Functor ((<&>))
+import Data.Maybe (mapMaybe)
 import Foreign.C.Types (CInt)
 import SDL (($=))
 import SDL qualified
@@ -72,14 +76,12 @@ renderWorld renderer world tileset =
     World.coordMap_ renderTile world
   where
     renderTile (x, y) tile =
-        let
-            source = Tiles.getRect tile
+        let source = Tiles.getRect tile
             dest =
                 SDL.Rectangle
                     (SDL.P $ SDL.V2 (Tiles.tileSize * fromIntegral x) (Tiles.tileSize * fromIntegral y))
                     Tiles.tileDimensions
-        in
-            SDL.copy renderer tileset (Just source) (Just dest)
+        in SDL.copy renderer tileset (Just source) (Just dest)
 
 escPressed evt =
     case SDL.eventPayload evt of
@@ -88,32 +90,52 @@ escPressed evt =
                 && SDL.keysymKeycode (SDL.keyboardEventKeysym keyboardEvent) == SDL.KeycodeEscape
         _ -> False
 
+updateInputState :: MVar ECS.InputState -> [SDL.KeyboardEventData] -> IO ()
+updateInputState inputState keyboardEvents =
+    forM_ keyboardEvents $ \evt -> do
+        let val = SDL.keyboardEventKeyMotion evt == SDL.Pressed
+        case SDL.keysymScancode (SDL.keyboardEventKeysym evt) of
+            -- FIXME: collect keydowns/ups and build state before updating
+            SDL.ScancodeDown -> modifyMVar_ inputState (\s -> pure s{ECS.down = val})
+            SDL.ScancodeUp -> modifyMVar_ inputState (\s -> pure s{ECS.up = val})
+            SDL.ScancodeLeft -> modifyMVar_ inputState (\s -> pure s{ECS.left = val})
+            SDL.ScancodeRight -> modifyMVar_ inputState (\s -> pure s{ECS.right = val})
+            _ -> pure ()
+
 gameLoop ::
     SDL.Renderer ->
     World.World ->
     SDL.Texture ->
+    MVar ECS.InputState ->
     [ECS.System] ->
     [ECS.Entity] ->
     IO ()
-gameLoop renderer world tileset systems entities =
+gameLoop renderer world tileset inputState systems =
     go
   where
-    go = do
+    go entities = do
         evts <- SDL.pollEvents
+        updateInputState inputState $
+            mapMaybe
+                ( \e -> case e.eventPayload of
+                    SDL.KeyboardEvent payload -> Just payload
+                    _ -> Nothing
+                )
+                evts
         SDL.rendererDrawColor renderer $= SDL.V4 20 20 25 150
         SDL.clear renderer
-        -- renderWorld renderer world tileset
-        traverse_ (\sys -> sys.run entities) systems
+        entities' <- foldM (\es sys -> sys.run es) entities systems
+        threadDelay 10_000
         SDL.present renderer
-        unless (any escPressed evts) go
+        unless (any escPressed evts) (go entities')
 
 mkFoo :: IO ECS.Entity
 mkFoo =
-    ECS.setHealth (ECS.Health 100) <$> ECS.mkEmptyEntity
+    ECS.mkEmptyEntity <&> ECS.setHealth (ECS.Health 100)
 
 main :: IO ()
 main = do
-    let globalScale = 10
+    let globalScale = 5
     world <- World.load "test.world"
     withSdl $
         withSdlWindow 2000 1800 $
@@ -121,10 +143,17 @@ main = do
                 withSdlImage [Image.PNG] $ do
                     withSdlRenderer window $ \renderer -> do
                         tileset <- Tiles.loadTileset renderer
+                        inputState <- newMVar ECS.initialInputState
                         SDL.rendererScale renderer $= SDL.V2 (realToFrac globalScale) (realToFrac globalScale)
                         let renderSystem = ECS.mkRenderSystem renderer tileset
-                        player <- ECS.setPosition (20, 20) . ECS.setAppearance ECS.Yellow <$> mkFoo
+                            inputSystem = ECS.mkInputSystem (readMVar inputState)
+                            systems = [inputSystem, renderSystem]
+                        player <-
+                            ECS.setPlayerControlled
+                                . ECS.setPosition (20, 20)
+                                . ECS.setAppearance ECS.Yellow
+                                <$> mkFoo
                         foo1 <- ECS.setPosition (80, 50) . ECS.setAppearance ECS.Red <$> mkFoo
                         foo2 <- ECS.setPosition (0, 90) . ECS.setAppearance ECS.Red <$> mkFoo
                         let entities = [player, foo1, foo2]
-                        gameLoop renderer world tileset [renderSystem] entities
+                        gameLoop renderer world tileset inputState systems entities
